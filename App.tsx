d03742d3aeec1ec5692a4e527ad8f31b9333bf1d
rmakotoinoue <rmakotoinoue@gmail.com>
2022-05-06 08:01:07 -0300

    fix stage 1

--- App.tsx
+++ App.tsx
@@ -1,9 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Widget } from './src/components/Widget';
 import { theme } from './src/theme';
+import React from 'react';
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+      Inter_400Regular,
+      Inter_500Medium
+    });
+  
+    if (!fontsLoaded) {
+      return <AppLoading />;
+    }
+
   return (
     <View style={
       {
